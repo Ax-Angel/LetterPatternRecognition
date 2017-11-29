d8eb79f35e0c39f0d32acb62a1467aa393e7ee54
sanchezmaxar <sanchezmaxar@gmail.com>
2017-11-28 18:40:58 -0600

    agrego readme y reporte

--- MLRFC.py
+++ MLRFC.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 from sklearn.datasets import load_digits
 from sklearn import ensemble
-import pylab as pl 	
+import pylab as pl
 import random
 
 digits = load_digits()
@@ -10,7 +10,7 @@ n_samples = len(digits.images)
 x = digits.images.reshape((n_samples, -1))
 y = digits.target
 
-#Create random indices 
+#Create random indices
 sample_index=random.sample(range(len(x)),len(x)/5) #20-80
 valid_index=[i for i in range(len(x)) if i not in sample_index]
 
@@ -30,11 +30,11 @@ classifier.fit(sample_images, sample_target)
 
 #Attempt to predict validation data
 score=classifier.score(valid_images, valid_target)
-print 'Random Tree Classifier:\n' 
+print 'Random Tree Classifier:\n'
 print 'Score\t'+str(score)
 print classifier.predict([x[i]])
 i=800
 
-pl.gray() 
-pl.matshow(digits.images[i]) 
+pl.gray()
+pl.matshow(digits.images[i])
 pl.show()
